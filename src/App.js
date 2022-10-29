7d015baafab0edc5b02343e5b9d2e1bfd196e5b5
Pratham Aditya Salhotra <prathamadityasalhotra@gmail.com>
2022-10-29 10:26:06 +0530

    project-website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "./components/NavigationBar/Navigation";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navigation></Navigation>
+    </React.Fragment>
+  );
+}
+
+export default App;
