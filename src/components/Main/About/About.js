5e67fdd30e35d77098ff76541c4b82b9c3cc4fd4
Pratham Aditya Salhotra <prathamadityasalhotra@gmail.com>
2022-10-31 20:28:41 +0530

    more code

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -1,17 +1,35 @@
 import React from "react";
 import "./About.css";
-import cone from "../../../assets/cone.png";
+import eyebase from "../../../assets/eyebase.png";
+import eyetop from "../../../assets/eyetop.png";
+import { useEffect, useState } from "react";
+
+const eyes = document.querySelectorAll(".eye-top");
+const anchor = docu;
+function MousePosition() {
+  const [mousePos, setMousePos] = useState({});
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePos({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return mousePos;
+}
 
 const About = () => {
   return (
     <React.Fragment>
-      <img
-        src={cone}
-        alt="the image of cone"
-        width="600"
-        height="600"
-        className="main-image"
-      />
+      <img src={eyebase} alt="eye-base" className="eye-base" />
+      <img src={eyetop} alt="eye-top-left" className="eye-top left" />
+      <img src={eyetop} alt="eye-top-right" className="eye-top right" />
       <h2 className="about-text">This is about us on main page.</h2>
     </React.Fragment>
   );
