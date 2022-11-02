f34357b0338c8e6effdf5c1a38276066067df7f0
Pratham Aditya Salhotra <prathamadityasalhotra@gmail.com>
2022-11-02 09:51:19 +0530

    sync

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -4,45 +4,40 @@ import eyebase from "../../../assets/eyebase.png";
 import eyetop from "../../../assets/eyetop.png";
 import { useEffect, useState } from "react";
 
-
-function angle(cx,cy,x,ey){
-  const dy=ey-cy;
-  const dx=x-cx;
-  const theta=Math.atan2(dy,dx);
-  return theta*180/Math.PI;
+function angle(cx, cy, x, ey) {
+  const dy = ey - cy;
+  const dx = x - cx;
+  const theta = Math.atan2(dy, dx);
+  return (theta * 180) / Math.PI;
 }
 
 const About = () => {
-  const [radi,setradi]=useState(0);
+  const [radi, setradi] = useState(0);
   document.addEventListener("mousemove", (e) => {
     const eye = document.getElementById("eyes");
-    const mouseX=e.clientX;
-    const mouseY=e.clientY;
-    const rekt=eye.getBoundingClientRect();
-    const eyeX=rekt.left+rekt.width/2;
-    const eyeY=rekt.top+rekt.height/2;
-    const rad=angle(eyeX,eyeY,mouseX,mouseY);
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const rekt = eye.getBoundingClientRect();
+    const eyeX = rekt.left + rekt.width / 2;
+    const eyeY = rekt.top + rekt.height / 2;
+    const rad = angle(eyeX, eyeY, mouseX, mouseY);
     setradi(rad);
-    const eyes= document.querySelectorAll(".eye-top");
-    console.log(radi);
+    const eyes = document.querySelectorAll(".eye-top");
     eyes.forEach((eye) => {
-      eye.style.transform = `rotate(${180+90+radi}deg)`;
+      eye.style.transform = `rotate(${180 + 90 + radi}deg)`;
     });
-  
   });
 
   return (
     <React.Fragment>
       <div className="main-eye-class">
-        <div id="eyes" >
-        <img src={eyebase} alt="eye-base" className="eye-base" />
-        <img src={eyetop} alt="eye-top-left" className="eye-top left" />
-        <img src={eyetop} alt="eye-top-right" className="eye-top right" />
+        <div id="eyes">
+          <img src={eyebase} alt="eye-base" className="eye-base" />
+          <img src={eyetop} alt="eye-top-left" className="eye-top left" />
+          <img src={eyetop} alt="eye-top-right" className="eye-top right" />
         </div>
         <h2 className="about-text">This is about us on main page.</h2>
       </div>
-
-      
     </React.Fragment>
   );
 };
